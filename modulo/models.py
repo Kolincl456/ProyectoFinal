0f65c6a0acaad1bd30046b9bfccbcecfd8d41f93
Kolincl456 <Kolincl456@Gmail.com>
2021-11-24 17:49:55 -0600

    Creaciòn del repositorio en git hub

---
+++ modulo/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Area(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(
+            default=timezone.now)
+    fecha_publicacion = models.DateTimeField(
+            blank=True, null=True)
+
+    def publicar(self):
+        self.fecha_publicacion = timezone.now()
+        self.save()
+
+    #def __str__(self):
+    ##    return self.title
