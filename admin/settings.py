73d1311d306be876412515300f34d365d974c0d3
Kolincl456 <Kolincl456@Gmail.com>
2021-11-24 18:32:45 -0600

    Renombre de las rutas del proyecto

--- admin/settings.py
+++ admin/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modulo.apps.ModuloConfig',
+    'modulos.apps.ModuloConfig',
 ]
 
 MIDDLEWARE = [
